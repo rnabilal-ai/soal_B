8392be315fd568bac5f1e019b8171e74065c40b9
rnabilal-ai <rifqinabilal25@gmail.com>
2024-11-25 15:40:05 +0700

    menambahkan idex dan css serta js

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beranda</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+<?php
+    include_once('navbar.php');
+?>
+
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-6 m-auto">
+            <div class="card">
+            <div class="card-header">
+                <h3>Event Manager</h3>
+            </div>
+            <div class="card-body">
+                <h4>Selamat Datang, User</h4>
+                <p>Yok Bisa lal harus semangat yaa ke pastii bisa kok akuu yakin karna ke udah sejauh ini jangan pernah lihat belakang AYOK SEMANGAT KE PASTI BISAA</p>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+    
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+</body>
+</html>
